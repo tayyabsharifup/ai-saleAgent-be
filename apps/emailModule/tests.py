9099b369aefc830ec0f61be16e2f9116eacd2480
Dawood Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2025-09-25 20:00:22 +0500

    test case for outlook

--- apps/emailModule/tests.py
+++ apps/emailModule/tests.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv
+import os
 
 load_dotenv(override=True)
 
 # Create your tests here.
+email = os.getenv('OUTLOOK')
+password = os.getenv('OUTLOOK_PASSWORD')
 
-
+from imap_tools import MailBox, AND, OR
+with MailBox('outlook.office365.com').login(email,password,'INBOX') as mailbox:
+    print( [msg for msg in mailbox.fetch(reverse=True)])
