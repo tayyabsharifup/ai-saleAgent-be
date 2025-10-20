7156fca72f91468a1bcc3df6dd83723949fef8fd
Dawood Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2025-10-20 20:06:43 +0500

    remove initial node, add products

--- apps/aiModule/utils/follow_up.py
+++ apps/aiModule/utils/follow_up.py
@@ -1,3 +1,15 @@
+from apps.aiModule.utils.output_extractor import InterestLevel, FollowUpPlan, MessageResponse, ShouldFollowUp
+from apps.aiModule.utils.util_model import get_chat_message_by_id, save_ai_message
+from apps.users.models.manager_model import ManagerModel
+from apps.users.models.lead_model import LeadModel
+from langchain_core.messages import AIMessage
+from IPython.display import Image
+from typing import Literal
+from langgraph.graph import StateGraph, START, END
+from pydantic import BaseModel
+from langchain_core.messages import SystemMessage
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
 import os
 # import sys
 import django
@@ -7,19 +19,6 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_sales.settings')
 django.setup()
 
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import SystemMessage
-from pydantic import BaseModel
-from langgraph.graph import StateGraph, START, END
-from typing import Literal
-from IPython.display import Image
-from langchain_core.messages import AIMessage
-
-from apps.users.models.lead_model import LeadModel
-from apps.users.models.manager_model import ManagerModel
-from apps.aiModule.utils.util_model import get_chat_message_by_id, save_ai_message
-from apps.aiModule.utils.output_extractor import InterestLevel, FollowUpPlan, MessageResponse, ShouldFollowUp
 
 load_dotenv(override=True)
 
@@ -30,6 +29,7 @@ class SalesState(BaseModel):
     body: str = ""
     follow_up_date: int = 0
     contact_type: str = ""
+
 
 class AITool:
     def __init__(self, lead_id):
@@ -70,13 +70,13 @@ class AITool:
 
             ]
         }
-    
 
     def initialDecideNode(self, state: SalesState) -> Literal['initial_response', 'classifier', '__end__']:
         if not self.messages:
-            return 'initial_response'
+            return END
         else:
-            llm = ChatOpenAI(model='gpt-4.1-mini', temperature=0).with_structured_output(ShouldFollowUp)
+            llm = ChatOpenAI(
+                model='gpt-4.1-mini', temperature=0).with_structured_output(ShouldFollowUp)
             system_prompt = f"""
                 You are Expert Follow Up planner. By giving the list of the Message interaction between Client, Agent and AI module.
                 There would two scenario to which write a follow-up plan.
@@ -95,51 +95,51 @@ class AITool:
             else:
                 return END
 
-    def initialResponseNode(self, state: SalesState):
-        system_prompt = """
+    # def initialResponseNode(self, state: SalesState):
+    #     system_prompt = """
 
-            """
-        try:
-            manager = ManagerModel.objects.get(agent_model__leadmodel__id=self.lead_id)
-            language = manager.language
-            offer = manager.offer
-            selling_point = manager.selling_point
-            faq = manager.faq
+    #         """
+    #     try:
+    #         manager = ManagerModel.objects.get(agent_model__leadmodel__id=self.lead_id)
+    #         language = manager.language
+    #         offer = manager.offer
+    #         selling_point = manager.selling_point
+    #         faq = manager.faq
 
-        except Exception as e:
-            language = ''
-            offer = ''
-            selling_point = ''
-            faq = ''
+    #     except Exception as e:
+    #         language = ''
+    #         offer = ''
+    #         selling_point = ''
+    #         faq = ''
 
-        llm = ChatOpenAI(model='gpt-4.1-mini', temperature=0).with_structured_output(MessageResponse)
-        system_prompt = f"""
-        You are AI sales Assistant and Based on the information About the Lead and Agent.
-        Generate Few Talking point for Lead Call
-        Info About Client: {LeadModel.objects.get(id=self.lead_id).info}
-        Language: {language}
-        Offer: {offer}
-        Selling Point: {selling_point}
-        FAQ: {faq}
-        """
-        system_message = SystemMessage(content=system_prompt)
-        messages = [system_message] + self.messages
-        response = llm.invoke(messages)
-        if response:
-            state.heading = response.heading
-            state.body = response.body
-            state.contact_type = 'call'
-            state.follow_up_date = 1
-            state.interest_level = 'none'
-            if not save_ai_message(self.lead_id, state):
-                raise Exception("Failed to save AI message")
-        else:
-            raise Exception("Failed to generate response") 
-        return state
+    #     llm = ChatOpenAI(model='gpt-4.1-mini', temperature=0).with_structured_output(MessageResponse)
+    #     system_prompt = f"""
+    #     You are AI sales Assistant and Based on the information About the Lead and Agent.
+    #     Generate Few Talking point for Lead Call
+    #     Info About Client: {LeadModel.objects.get(id=self.lead_id).info}
+    #     Language: {language}
+    #     Offer: {offer}
+    #     Selling Point: {selling_point}
+    #     FAQ: {faq}
+    #     """
+    #     system_message = SystemMessage(content=system_prompt)
+    #     messages = [system_message] + self.messages
+    #     response = llm.invoke(messages)
+    #     if response:
+    #         state.heading = response.heading
+    #         state.body = response.body
+    #         state.contact_type = 'call'
+    #         state.follow_up_date = 1
+    #         state.interest_level = 'none'
+    #         if not save_ai_message(self.lead_id, state):
+    #             raise Exception("Failed to save AI message")
+    #     else:
+    #         raise Exception("Failed to generate response")
+    #     return state
 
-            
     def interestNode(self, state: SalesState):
-        llm = ChatOpenAI(model='gpt-4.1-mini', temperature=0).with_structured_output(InterestLevel)
+        llm = ChatOpenAI(model='gpt-4.1-mini',
+                         temperature=0).with_structured_output(InterestLevel)
         system_prompt = """
         Analyze the following sales conversation and classify the prospect's interest level into exactly one of these categories:
         
@@ -161,10 +161,11 @@ class AITool:
         else:
             state.interest_level = 'none'
         return state
-    
+
     def responseNode(self, state: SalesState):
         try:
-            manager = ManagerModel.objects.get(agent_model__leadmodel__id=self.lead_id)
+            manager = ManagerModel.objects.get(
+                agent_model__leadmodel__id=self.lead_id)
             language = manager.language
             offer = manager.offer
             selling_point = manager.selling_point
@@ -176,7 +177,8 @@ class AITool:
             selling_point = ''
             faq = ''
 
-        llm = ChatOpenAI(model='gpt-4.1-mini', temperature=0).with_structured_output(MessageResponse)
+        llm = ChatOpenAI(model='gpt-4.1-mini',
+                         temperature=0).with_structured_output(MessageResponse)
         system_prompt = f"""
         Based on the prospect's interest level, generate a follow-up response
         It would be one of the following:
@@ -189,6 +191,58 @@ class AITool:
         Offer: {offer}
         Selling Point: {selling_point}
         FAQ: {faq}
+
+        and here are products details:
+            MoveAround
+            Operated with a joystick from the basket
+
+            MoveAround MA60
+            Working Height: 6m
+            Platform Size: 0.53m x 0.76m
+            Weight: 466 kg
+            Platform load: 150 kg
+
+            MoveAround MA50
+            Working Height: 5m
+            Platform Size: 0.53m x 0.76m
+            Weight: 331 kg
+            Platform load: 150 kg
+
+            MoveAround MA50-R
+            Working Height: 5m
+            Platform Size: 0.56m x 0.52m
+            Weight: 343 kg
+            Platform load: 150 kg
+
+            PushAround
+            Manually moved to where it's needed
+
+            PushAround PA60
+            Working Height: 6m
+            Platform Size: 0.53m x 0.76m
+            Weight: 466 kg
+            Platform load: 150 kg
+
+            PushAround PA50
+            Working Height: 5m
+            Platform Size: 0.53m x 0.76m
+            Weight: 331 kg
+            Platform load: 150 kg
+
+            PushAround PA35
+            Working Height: 5m
+            Platform Size: 0.56m x 0.52m
+            Weight: 343 kg
+            Platform load: 150 kg
+
+            StockPicker
+            Drivable and has height-adjustable lifting table
+
+            StockAround SP50
+            Working Height: 5m
+            Platform Size: 0.63m x 0.59m
+            Weight: 386 kg
+            Platform load: 165 kg
         """
         system_message = SystemMessage(content=system_prompt)
         messages = [system_message] + self.messages
@@ -203,7 +257,8 @@ class AITool:
         return state
 
     def followUpNode(self, state: SalesState):
-        llm = ChatOpenAI(model='gpt-4.1', temperature=0).with_structured_output(FollowUpPlan)
+        llm = ChatOpenAI(
+            model='gpt-4.1', temperature=0).with_structured_output(FollowUpPlan)
         system_prompt = """
             You are AI-Assistant for Sales Agent.
             You are generating a follow-up response for Sales Agent to the client. 
@@ -253,7 +308,7 @@ class AITool:
         system_message = SystemMessage(content=system_prompt)
         messages = [system_message] + self.messages
         response = llm.invoke(messages)
-        
+
         if response and isinstance(response, FollowUpPlan):
             state.follow_up_date = int(response.days)
             state.contact_type = response.contact_type
@@ -261,35 +316,29 @@ class AITool:
             state.follow_up_date = 0
             state.contact_type = 'none'
 
-
         return state
-        
 
-    
     def _build_graph(self):
         # Building the state graph
         builder = StateGraph(SalesState)
         # builder.add_node('initial_decide', self.initialDecideNode)
-        builder.add_node('initial_response', self.initialResponseNode)
+        # builder.add_node('initial_response', self.initialResponseNode)
         builder.add_node('classifier', self.interestNode)
         builder.add_node('follow_up', self.followUpNode)
         builder.add_node('response', self.responseNode)
 
-
         builder.add_conditional_edges(START, self.initialDecideNode)
-        builder.add_edge('initial_response', END)
+        # builder.add_edge('initial_response', END)
         builder.add_edge('classifier', 'follow_up')
         builder.add_edge('follow_up', 'response')
         builder.add_edge('response', END)
         return builder.compile()
-    
-    
+
 
 def refreshAI(lead_id):
     ai_tool = AITool(lead_id)
     final_state = ai_tool.graph.invoke(SalesState())
     return final_state
-
 
 
 if __name__ == "__main__":
@@ -302,7 +351,6 @@ if __name__ == "__main__":
     #     f.write(image_data)
     # print("Graph image saved to follow_up_graph.png")
     # Image(image_data)
-
 
     # print(ai_tool.messages)
     # if isinstance(ai_tool.messages[-1], AIMessage):
