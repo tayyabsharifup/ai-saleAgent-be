474b08e0f4a5eb5db0003610a9545387225805aa
Dawood Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2025-11-20 16:16:03 +0500

    feat(notifications): add async notification sending utility
    Add a new utility function to send notifications asynchronously using Django Channels,
    creating a notification model instance and broadcasting it via WebSocket to the user's group.

---
+++ apps/notifications/utils/send_async_notification.py
@@ -0,0 +1,30 @@
+
+from apps.notifications.models import NotificationModel
+from django.contrib.auth import get_user_model
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from apps.notifications.serializers import NotificationSerializer
+
+from typing import Tuple
+
+User = get_user_model()
+
+def send_async_notification(user: int, message: str) -> Tuple[bool, str]:
+    try:
+        user = User.objects.get(id=user)
+        notification = NotificationModel.objects.create(user=user, message=message)
+        serialzer = NotificationSerializer(notification)
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            f'notifications_{user.id}',
+            {
+                'type': 'send_notification',
+                'value': serialzer.data
+            }
+        )
+
+        return True, 'Notification sent successfully'
+    except Exception as e:
+        return False, str(e)
+    except User.DoesNotExist:
+        return False, f"User with id {user} does not exist"
